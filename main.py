6be9fd9f52e6bb419f2a12221a9d9178b8bb0b08
Alexandre Lambin <alexandre.lambin@gmail.com>
2019-08-03 11:13:30 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import py_xsd._drv as drv
+import py_xsd._drv2 as drv2
+import py_xsd.binding as xsd
+
+### Some tutorial
+###   https://www.liquid-technologies.com/xml-schema-tutorial/xsd-extending-types
+###   https://www.codesynthesis.com/projects/xsd/documentation/cxx/tree/manual/#2.11
+###   https://stackoverflow.com/questions/39868769/xsd-element-substitution-group-example
+
+class Foo:
+    def __init__(self):
+        self.male = xsd.gender_t('male')
+        self.female = xsd.gender_t('female')
+        self.doc = xsd.root()
+
+    def add_default_person(self):
+        p = xsd.person_t()
+        p.id = 1
+        p.age = 32
+        p.first_name = "Alex"
+        p.last_name = "L"
+        p.gender = self.male
+        self.doc.person.append(p)
+
+    def add_default_derived(self):
+        d = drv.derived()
+        d.extra_text = "Hello World!"
+        d.var1 = "my value 1"
+        self.doc.item.append(d)
+
+    def add_default_derived2(self):
+        d = drv2.derived2()
+        d.extra_text = "Hello Universe!"
+        d.var3 = "my perfect value 1"
+        self.doc.item.append(d)
+
+    def gen(self):
+        f = open("output.xml", "wb")
+        f.write(self.doc.toxml("utf-8", element_name='config'))
+        print("done")
+        f.close()
+
+def test():
+    f = Foo()
+    f.add_default_person()
+    f.add_default_derived()
+    f.add_default_derived2()
+    f.gen()
+
+def load():
+    rxml = open('xsd/people.xml').read()
+    doc = xsd.CreateFromDocument(rxml)
+    print("{}".format(doc))
+
+if __name__ == '__main__':
+    print("Hello World!")
+    test()
---
+++ readme.md
@@ -0,0 +1,32 @@
+XSD Python Example
+==================
+
+
+Branch availables
+-----------------
+
+* simple_extension
+    Use extension and xsi:type
+    
+Example of XML output
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<config xmlns:ns1="http://NamespaceTest.com/DerivedTypes" xmlns:ns2="http://NamespaceTest.com/Derived2Types"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <person id="1">
+        <first-name>Alex</first-name>
+        <last-name>L</last-name>
+        <gender>male</gender>
+        <age>32</age>
+    </person>
+    <item extra-text="Hello World!" xsi:type="ns1:derived">
+        <ns1:var1>my value 1</ns1:var1>
+    </item>
+    <item extra-text="Hello Universe!" xsi:type="ns2:derived2">
+        <ns2:var3>my perfect value 1</ns2:var3>
+    </item>
+</config>
+```
+
+
+* example_2
